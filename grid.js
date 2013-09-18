5d21fb12c3a266fcc11d38ee95df3f9996aafc3a
Emrah <emrahb@gmail.com>
2013-09-18 14:50:18 +0300

    Added preliminary paging, and paging related calculations

--- grid.js
+++ grid.js
@@ -21,16 +21,30 @@
 
    var i18n = {
       filter: "Aranacak metni giriniz...",
-      total: "Toplam",
-      prev: "Önceki",
-      next: "Sonraki"
+      first: "İlk sayfa",
+      last: "Son sayfa",
+      next: "Sonraki",
+      previous: "Önceki",
+      total: "Toplam"
    }
 
    var defaultGridOptions = {
+      deepLinking: true,
       searching : true,
       selectable: true,
       sorting: true,
-      paging: true
+      paging: true,
+      pageSize: 3
+   }
+
+   // ng-repeat does not accept ranges as of now, need to provide a collection
+   var generateNumberArray = function(numberOfItems) {
+      var numberArray = [];
+      for(i = 1; i <= numberOfItems ; i++) {
+         numberArray.push(i);
+      }
+
+      return numberArray;
    }
 
    gridModule.directive("grid", function($filter) {
@@ -50,29 +64,68 @@
             // apply display filters
             scope.transformedData = applyFilters(scope.data, scope.columns, scope.$eval);
             scope.i18n = i18n;
-            
+
             scope.options = defaultGridOptions;
+            scope.pageIndex = 1;
 
             scope.sort = function(column) {
                scope.reverseOrder = scope.sortExpression == column.field && !scope.reverseOrder;
-               
                scope.sortExpression = column.field;
-            }
+            };
 
             scope.onClick = function(row) {
                scope.toggleRow(row);
                scope.rowClick();
-            }
+            };
 
             scope.onDoubleClick = function(row) {
                scope.rowDoubleClick();
-            }
+            };
 
             scope.toggleRow = function(row) {
                scope.selectedRow = row;
+            };
+
+            scope.numberOfPages = function() {
+               if(!scope.filteredData()) return 0;
+
+               return Math.ceil(scope.filteredData().length / scope.options.pageSize);
+            };
+
+            scope.filteredData = function() {
+               return scope.$eval("transformedData | orderBy:sortExpression:reverseOrder | filter:searchText");
             }
+
+            scope.pageData = function(pageNumber) {
+               if(pageNumber < 1) pageNumber = 1;
+
+               var lowerBound = (pageNumber - 1) * scope.options.pageSize;
+               var upperBound = lowerBound + scope.options.pageSize;
+
+               var pageData = [];
+
+               if(scope.filteredData().length > 0)
+                  pageData = scope.filteredData().slice(lowerBound, upperBound);
+
+               return pageData;
+            }
+
+            scope.first = function(){
+               scope.pageIndex = 1;
+            };
+
+            scope.prev = function(){
+               scope.pageIndex = Math.max(scope.pageIndex - 1, 1);
+            };
+
+            scope.last = function(){
+               scope.pageIndex = scope.numberOfPages();
+            };
+
+            scope.next = function(){
+               scope.pageIndex = Math.min(scope.pageIndex + 1, scope.numberOfPages());
+            };
          }  
       }
    });
-
 })(window.angular);
--- test/app.js
+++ test/app.js
@@ -4,16 +4,16 @@ app.controller("TestGridCtrl", function($scope) {
    $scope.employees = [
       {employeeId: 1, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
       {employeeId: 2, firstName: "Ali", lastName: "Veli", department: "Muhasebe", birthday: 450997200000, salary: 123123},
-      {employeeId: 2, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
-      {employeeId: 4, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
-      {employeeId: 5, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
-      {employeeId: 6, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
-      {employeeId: 1, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
-      {employeeId: 2, firstName: "Ali", lastName: "Veli", department: "Muhasebe", birthday: 450997200000, salary: 123123},
       {employeeId: 3, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
       {employeeId: 4, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
       {employeeId: 5, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
-      {employeeId: 6, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123}
+      {employeeId: 6, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
+      {employeeId: 7, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
+      {employeeId: 8, firstName: "Ali", lastName: "Veli", department: "Muhasebe", birthday: 450997200000, salary: 123123},
+      {employeeId: 9, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123},
+      {employeeId: 10, firstName: "John", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
+      {employeeId: 11, firstName: "Zohn", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123},
+      {employeeId: 12, firstName: "Şeyma", lastName: "Smith", department: "Legal", birthday: 450997200000, salary: 123123}
    ];
 
    $scope.columns = [
@@ -22,6 +22,8 @@ app.controller("TestGridCtrl", function($scope) {
       {title:"Last Name", field: "lastName"},
       {title:"Department", field: "department"},
       {title: "Birthday", field: "birthday", filter: "date:'dd.MM.yyyy'"},
-      {title: "Salary", field: "salary", filter: "currency:'$'"}
+      {title: "Salary", field: "salary", filter: "currency:'$ '"}
    ];
+
+   $scope.gridOptions = {};
 });
